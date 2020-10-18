036c5a74c9b7722464670e139d93b038735e6f6e
gehpaw16 <gehpaw16@gmail.com>
2020-10-18 23:12:28 +0530

    Python Files added

---
+++ Number series horizontal.py
@@ -0,0 +1,6 @@
+''' Print 1 	2 	3 	4 	5 	6 	7 	8 	9 	10 '''
+a=1
+print("Series question 1")
+while(a<=10):
+ print(a,'\t', end = '')
+ a=a+1
---
+++ vertical string print.py
@@ -0,0 +1,9 @@
+''' Display string in vertical manner'''
+
+
+i=0
+print('Enter the string')
+a=input()
+while(i<len(a)):
+  print(a[i])
+  i+=1
